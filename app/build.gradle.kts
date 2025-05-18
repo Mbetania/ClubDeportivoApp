b6969334b10b86cbc698492850c780c07c08d5e2
Pablo M. Iglesias <pmiglesias@live.com>
2025-05-17 21:44:38 -0300

    new branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,13 +43,11 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation (libs.material.v160)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
 }
